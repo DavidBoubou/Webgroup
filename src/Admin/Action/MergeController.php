7315d38e0c7b5187d7f2b1fc95cd59a52b574f9b
unknown <david.boudine0@gmail.com>
2022-12-09 01:43:59 +0100

    Update my bach action

--- src/Admin/Action/MergeController.php
+++ src/Admin/Action/MergeController.php
@@ -15,8 +15,32 @@ class MergeController extends AbstractController
 {
     public function batchMergeAction(ProxyQueryInterface $query, AdminInterface $admin,ChartBuilderInterface $chartBuilder): RedirectResponse
     {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
 
+        // Create data with articles realease by my user.
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
 
+        //Option du chart
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+
+            //My special template with menu and chart.
             $this->addFlash('sonata_flash_success', 'flash_batch_merge_success');
             return new RedirectResponse('/' );
     }
